78788be1b486a7e2df9ca532cd458c84e53a3c62
cx <473427618@qq.com>
2024-04-14 13:48:48 +0800

    添加协议

---
+++ proto/generate_java.bat
@@ -0,0 +1,1 @@
+E:\java-gameserver\java-gameserver\proto\protoc.exe --java_out=E:\java-gameserver\java-gameserver\game-common\src\main\java --proto_path=E:\java-gameserver\java-gameserver\proto\ E:\java-gameserver\java-gameserver\proto\OutMessage.proto
